5a1b199834ddff13a1b2f6b6f50906676f85e839
buffashe <buffashe.com@gmail.com>
2022-04-05 03:57:01 +0900

    null

---
+++ acmicpc/1330/1330.cc
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+    
+    if (a > b) {
+        cout << ">";
+    }
+    else if (a == b) {
+        cout << "==";
+    }
+    else {
+        cout << "<";
+    }
+    return 0;
+}
